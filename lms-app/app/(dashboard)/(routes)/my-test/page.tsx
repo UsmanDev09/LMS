f3d9a8ca9926df01cf107bb9f53c030c5dc0c97b
Muhammad Sualeh <sualeh.naeem@gmail.com>
2024-02-13 17:46:11 +0500

    Finished CLerk Authentication

---
+++ lms-app/app/(dashboard)/(routes)/my-test/page.tsx
@@ -0,0 +1,5 @@
+const TestPage = () => {
+  return <div>This Must not be protected</div>;
+};
+
+export default TestPage;
--- lms-app/app/(dashboard)/(routes)/page.tsx
+++ lms-app/app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-
+// import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="text-sky-700 text-3xl">
       <h1>This is a Home Page</h1>
-      <Button variant="destructive"> Click Me </Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
