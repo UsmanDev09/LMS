720ae5861ffd776c5d19449b8310d2b75822977a
Muhammad Sualeh <sualeh.naeem@gmail.com>
2024-02-14 15:37:15 +0500

    Dynamic Layout Complete

--- lms-app/app/(dashboard)/(routes)/search/page.tsx
+++ lms-app/app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,5 @@
 const SearchPage = () => {
-  return (
-    <div className="flex justify-center text-center">This is a Search Page</div>
-  );
+  return <div>This is a Search Page</div>;
 };
 
 export default SearchPage;
